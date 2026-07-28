dc0bf6bceea8d1ac604a40e2ae8a114f9ca95c80
kuthayoga295 <kuthayoga295@gmail.com>
2026-07-28 17:52:05 +0800

    update

--- pages/poliklinik/data.js
+++ pages/poliklinik/data.js
@@ -64,7 +64,7 @@ async function initPoli() {
             <h3 class="text-success">${poli.nama}</h3>
             <h6 class="text-muted">${poli.deskripsi}</h6>
           </div>
-          <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-5 g-4 py-2">
+          <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4 py-2">
             ${doctorCardsHTML}
           </div>
         </div>
